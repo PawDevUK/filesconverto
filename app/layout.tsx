1ab40397be913af1bd16f841e718155159cdae4d
GitcrackerUK <pfsiwek@gmail.com>
2025-06-26 18:14:45 +0100

    Changed style path.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/styles/globals.css"; // Ensure the path is correct
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
