b4cdf4086b1de242aef22e4e84642dab90212325
GitcrackerUK <pfsiwek@gmail.com>
2025-06-29 23:01:37 +0100

    Converted '

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -113,7 +113,7 @@ export default function LoginPage() {
         </div>
       </div>
       <p className="mt-4 text-center text-sm text-gray-600">
-        Don't have an account? 
+        Don&apos;t have an account? 
         <a href="#" className="text-blue-600 hover:underline font-bold ml-[10px]">
           Get access →
         </a>
