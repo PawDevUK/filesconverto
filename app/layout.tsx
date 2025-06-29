1cd64df0f50faf185346dec1b2f8ef90608909b2
GitcrackerUK <pfsiwek@gmail.com>
2025-06-29 21:54:25 +0100

    Moved page elements to main layout as it possibly will be used across pages.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,33 @@
-import "@/app/styles/globals.css"; // Ensure the path is correct
+import '@/app/styles/globals.css';
+import React from 'react';
+import Head from 'next/head';
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
 
+import { store } from '@/app/store/formats';
+const companyName = store.companyName || 'FilesConverto';
 export const metadata = {
-  title: 'Files Converter',
-  icons: {
-    icon: '/favicon.ico',
-  },
+	title: 'Files Converter',
+	icons: {
+		icon: '/favicon.ico',
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body className={`antialiased`}>
+				<Head>
+					<title>Free Files Converter</title>
+				</Head>
+				<Header companyName={companyName}></Header>
+				{children}
+				<Footer companyName={companyName}></Footer>
+			</body>
+		</html>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,17 @@
 'use client';
 import React from 'react';
-import Head from 'next/head';
-import Header from './components/Header';
 import Hero from './components/Hero';
-import Footer from './components/Footer';
 
 type HomeProps = {
-  companyName: string,
+	companyName: string;
 };
 
-const Home: React.FC<HomeProps> = ({ companyName="FilesConverto" }) => {
+const Home: React.FC<HomeProps> = () => {
 	return (
 		<div className='min-h-screen bg-gray-50 text-gray-800'>
-			<Head>
-				<title>Free Files Converter</title>
-			</Head>
-
-			<Header companyName={companyName}></Header>
 			<Hero></Hero>
-
-			{/* Features Section */}
-
-			{/* Footer */}
-			<Footer companyName={companyName}></Footer>
 		</div>
 	);
-}
+};
 
 export default Home;
