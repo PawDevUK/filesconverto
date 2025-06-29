a3eb88bf854e983d734f56001a27a59e200d9adc
GitcrackerUK <pfsiwek@gmail.com>
2025-06-29 21:43:32 +0100

    Removed login layout as it should use main one.

--- app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-100 flex items-center justify-center min-h-screen">
-        <main className="w-full max-w-md p-8 bg-white rounded shadow">{children}</main>
-      </body>
-    </html>
-  );
-}
