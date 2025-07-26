5b130376d7473541635d9aa5f8eb337e089588b8
pawel <pfsiwek@gmail.com>
2025-07-26 07:21:49 +0100

    Removed pdf-converter from the project as conversion will be done by external server.

--- app/api/pdf/convert/route.ts
+++
@@ -1,7 +0,0 @@
-import { pdfAPIHandler } from '@/lib/pdf-converter/api-handler';
-import { NextRequest } from 'next/server';
-
-export async function POST(request: Request) {
-  const nextRequest = new NextRequest(request);
-  return pdfAPIHandler.handleConvert(nextRequest);
-}
