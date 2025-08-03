17d038bf969e25cc293b248ecd2cc76272d87926
pawel <pfsiwek@gmail.com>
2025-08-03 21:45:52 +0100

    Updated Card.

--- app/PremiumPrices/page.tsx
+++ app/PremiumPrices/page.tsx
@@ -2,17 +2,24 @@
 import React, {useEffect} from 'react';
 import MainPage from '../pages/main';
 import PricingCard from '../components/ui/card';
+import{ price_Plans, pricePlans }from 'app/store/data'
+import './premium.css'
 
-function PremiumPrices() {
+const PremiumPrices:React.FC<price_Plans>=() =>{
   useEffect(()=>{
 
   },[])
   // const [state, setState] = useState()
   return (
    <MainPage>
-    {<div>
-      <PricingCard></PricingCard>
-    </div>}
+    <div className='cards-wrapper'>
+      {
+      pricePlans.map((plan, index)=>{
+      return <PricingCard key={index}
+      {...plan} ></PricingCard>
+      })
+      }
+    </div>
    </MainPage>
   )
 }
