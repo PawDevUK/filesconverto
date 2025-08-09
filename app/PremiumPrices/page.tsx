37f11e568406f49ba2f069c753c92bcea8b4a499
pawel <pfsiwek@gmail.com>
2025-08-09 14:35:40 +0100

    Added responsive width tto wrapper and cards.

--- app/PremiumPrices/page.tsx
+++ app/PremiumPrices/page.tsx
@@ -12,13 +12,10 @@ const PremiumPrices:React.FC<price_Plans>=() =>{
   // const [state, setState] = useState()
   return (
    <MainPage>
-    <div className='cards-wrapper'>
-      {
-      pricePlans.map((plan, index)=>{
-      return <PricingCard key={index}
-      {...plan} ></PricingCard>
-      })
-      }
+    <div className="cards-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center">
+      {pricePlans.map((plan, index) => (
+        <PricingCard key={index} {...plan} />
+      ))}
     </div>
    </MainPage>
   )
