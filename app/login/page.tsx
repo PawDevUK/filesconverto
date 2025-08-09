952ba403d428842efa5ae758a432202a27c24383
pawel <pfsiwek@gmail.com>
2025-08-09 14:49:44 +0100

    Changed GH into FB icon and text.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 import MainPage from "../pages/main";
 
-import GH from "@/public/github-mark.svg"
+import FB from "@/public/Facebook.png"
 import GOOGLE from "@/public/google.svg";
 
 export default function LoginPage() {
@@ -107,8 +107,8 @@ export default function LoginPage() {
               href="#"
               className="flex items-center justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
             >
-              <Image src={GH} alt="GitHub" className="h-5 w-5 mr-2" />
-              <span className="text-sm font-medium">GitHub</span>
+              <Image src={FB} alt="Facebook" className="h-5 w-5 mr-2" />
+              <span className="text-sm font-medium">Facebook</span>
             </a>
           </div>
         </div>
