c3e3722c37012091d4b7ae0626d3251156c3dfe2
pawel <pfsiwek@gmail.com>
2025-08-09 15:35:25 +0100

    Added  FairPricing.tsx

---
+++ app/PremiumPrices/FairPricing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FairPricing: React.FC = () => (
+  <div className="">
+    <div className="max-w-5xl mx-auto text-center px-4">
+      <p className="mb-2 mt-1 mx-3 text-base md:text-lg text-gray-700 dark:text-gray-200">
+        You can use FilesConverto absolutely free for up to 10 conversions per day. Beyond that we
+        offer flexible payment options. Use the quantity slider below to customize a package or subscription according to your needs.
+      </p>
+    </div>
+  </div>
+);
+
+export default FairPricing;
--- app/PremiumPrices/page.tsx
+++ app/PremiumPrices/page.tsx
@@ -2,6 +2,7 @@
 import React, {useEffect} from 'react';
 import MainPage from '../pages/main';
 import PricingCard from '../components/ui/card';
+import FairPricing from '@/app/PremiumPrices/FairPricing';
 import{ price_Plans, pricePlans }from 'app/store/data'
 import './premium.css'
 
@@ -12,6 +13,7 @@ const PremiumPrices:React.FC<price_Plans>=() =>{
   // const [state, setState] = useState()
   return (
    <MainPage>
+    <FairPricing></FairPricing>
     <div className="cards-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center">
       {pricePlans.map((plan, index) => (
         <PricingCard key={index} {...plan} />
