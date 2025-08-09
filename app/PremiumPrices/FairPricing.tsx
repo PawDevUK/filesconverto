af6383b05b478dd4afde99cfb22f632e59323058
pawel <pfsiwek@gmail.com>
2025-08-09 18:35:38 +0100

    Rephrased the text.

--- app/PremiumPrices/FairPricing.tsx
+++ app/PremiumPrices/FairPricing.tsx
@@ -4,8 +4,7 @@ const FairPricing: React.FC = () => (
   <div className="">
     <div className="max-w-5xl mx-auto text-center px-4">
       <p className="mb-2 mt-1 mx-3 text-base md:text-lg text-gray-700 dark:text-gray-200">
-        You can use FilesConverto absolutely free for up to 10 conversions per day. Beyond that we
-        offer flexible payment options. Use the quantity slider below to customize a package or subscription according to your needs.
+        FilesConverto provides up to 15 conversions per day at no cost. For additional conversions, we offer a variety of flexible payment options. Please use the quantity slider below to select a package or subscription that best fits your needs.
       </p>
     </div>
   </div>
