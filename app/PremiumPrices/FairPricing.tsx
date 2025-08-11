06537a60c99da9086fc943fae48197b83092e69b
pawel <pfsiwek@gmail.com>
2025-08-11 21:31:22 +0100

    Removed dark theme font color.

--- app/PremiumPrices/FairPricing.tsx
+++ app/PremiumPrices/FairPricing.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FairPricing: React.FC = () => (
   <div className="">
     <div className="max-w-5xl mx-auto text-center px-4">
-      <p className="mb-2 mt-1 mx-3 text-base md:text-lg text-gray-700 dark:text-gray-200">
+      <p className="mb-2 mt-1 mx-3 text-base md:text-lg text-gray-700">
         FilesConverto provides up to 15 conversions per day at no cost. For additional conversions, we offer a variety of flexible payment options. Please use the quantity slider below to select a package or subscription that best fits your needs.
       </p>
     </div>
