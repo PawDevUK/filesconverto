491cd7f59142bc7d8919fa4b990136b44b9d0b94
pawel <pfsiwek@gmail.com>
2025-08-17 21:01:29 +0100

    Updated setup to avoid Links warnings in terminal. (safe to ignore)

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom';
+import 'jest-axe/extend-expect';
+import React from 'react'
+
+jest.mock('next/link', () => {
+  const Link = React.forwardRef<HTMLAnchorElement, React.AnchorHTMLAttributes<HTMLAnchorElement> & { href: string }>(
+    ({ children, href, ...rest }, ref) =>
+      React.createElement('a', { href, ref, ...rest }, children)
+  );
+  Link.displayName = 'Link';
+  return { __esModule: true, default: Link };
+});
