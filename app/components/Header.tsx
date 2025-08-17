84b13751d4969b940721638880b1b01e09751bbe
pawel <pfsiwek@gmail.com>
2025-08-17 21:01:53 +0100

    Accessibility fix.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -54,7 +54,7 @@ const Header: React.FC<HeaderProps> = ({}) => {
 					</GreenButton>
 				</div>
 				<div className='md:hidden'>
-					<button onClick={() => setIsMenuOpen(!isMenuOpen)} className='text-gray-700 focus:outline-none'>
+					<button onClick={() => setIsMenuOpen(!isMenuOpen)} aria-label='button' className='text-gray-700 focus:outline-none'>
 						<svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
 							<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d={isMenuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16M4 18h16'} />
 						</svg>
