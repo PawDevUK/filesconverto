3e75447d4e5874cdaaa7ce14a7dc4a38602b2121
GitcrackerUK <pfsiwek@gmail.com>
2025-11-21 09:24:45 +0000

    Refactor PremiumPrices component for improved readability and consistency

--- app/PremiumPrices/page.tsx
+++ app/PremiumPrices/page.tsx
@@ -1,32 +1,30 @@
-"use client"
-import React, {useEffect} from 'react';
+'use client';
+import React, { useEffect } from 'react';
 import MainPage from '../pages/main';
 import PricingCard from '../components/ui/card';
-import InPaidPlan from '@/app/PremiumPrices/InPaidPlan';
-import FairPricing from '@/app/PremiumPrices/FairPricing';
-import SecuredPayments from '@/app/PremiumPrices/SecuredPayments';
-import UsedBy from './UsedBy'
-import { pricePlans } from 'app/store/data'
-import './premium.css'
+import InPaidPlan from './InPaidPlan';
+import FairPricing from './FairPricing';
+import SecuredPayments from './SecuredPayments';
+import UsedBy from './UsedBy';
+import { pricePlans } from 'app/store/data';
+import './premium.css';
 
 const PremiumPrices: React.FC = () => {
-  useEffect(()=>{
-
-  },[])
-  // const [state, setState] = useState()
-  return (
-   <MainPage>
-    <FairPricing></FairPricing>
-    <div className="cards-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center">
-      {pricePlans.map((plan, index) => (
-        <PricingCard key={index} {...plan} />
-      ))}
-    </div>
-    <InPaidPlan></InPaidPlan>
-    <SecuredPayments></SecuredPayments>
-    <UsedBy></UsedBy>
-   </MainPage>
-  )
-}
+	useEffect(() => {}, []);
+	// const [state, setState] = useState()
+	return (
+		<MainPage>
+			<FairPricing></FairPricing>
+			<div className='cards-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-8 justify-items-center'>
+				{pricePlans.map((plan, index) => (
+					<PricingCard key={index} {...plan} />
+				))}
+			</div>
+			<InPaidPlan></InPaidPlan>
+			<SecuredPayments></SecuredPayments>
+			<UsedBy></UsedBy>
+		</MainPage>
+	);
+};
 
 export default PremiumPrices;
